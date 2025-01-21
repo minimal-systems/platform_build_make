8ed49e4c57ad25d5e7a4f35f31207ed9dd70b277
Kai Jones <kvjones0243@gmail.com>
2025-01-21 00:36:55 -0800

    build: define PWD variable to capture the absolute path of the working directory

---
+++ core/main.mk
@@ -0,0 +1,4 @@
+# Absolute path of the present working direcotry.
+# This overrides the shell variable $PWD, which does not necessarily points to
+# the top of the source tree, for example when "make -C" is used in m/mm/mmm.
+PWD := $(shell pwd)
