e84e8da688af08d71bee1a8d116796d9a57e6c05
Kai Jones <kvjones0243@gmail.com>
2025-01-21 00:38:00 -0800

    build: set default target to linux in main.mk

--- core/main.mk
+++ core/main.mk
@@ -2,3 +2,11 @@
 # This overrides the shell variable $PWD, which does not necessarily points to
 # the top of the source tree, for example when "make -C" is used in m/mm/mmm.
 PWD := $(shell pwd)
+
+# This is the default target.  It must be the first declared target.
+.PHONY: linux
+DEFAULT_GOAL := linux
+$(DEFAULT_GOAL): linux_targets
+
+.PHONY: linux_targets
+linux_targets:
